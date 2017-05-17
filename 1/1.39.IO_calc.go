26d6c3cc474656d06c727d58338e99e1f57e9b0d
me <me@me.com>
2017-05-17 14:24:29 +0700

    1.39. составить программу вычисления функции

--- 1/1.39.IO_calc.go
+++ 1/1.39.IO_calc.go
@@ -13,3 +13,26 @@
 	q = 2.8sin(x)+|y|
 		
 */
+
+
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"math"
+	"time"
+)
+
+func get(min, max int) (int) { // генерация псевдослучайных (!!! - не путать с действительно случайными) чисел
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	rand.Seed(time.Now().Unix()) // "сеим" случайности в основоной функции
+	x := float64(get(1, 500)) 
+  	y := float64(get(501, 1000)) 
+	fmt.Printf("%s%.0f\n%s%.0f\n", "x= ",  x, "y= ", y)
+	fmt.Printf("%s%.8f\n", "z= ", (x+(2+y)/math.Pow(x, 2))/(y + (1/(math.Sqrt(math.Pow(x, 2)+10)))))
+	fmt.Printf("%s%.8f\n", "q= ", 2.8*math.Sin(x) + math.Abs(y))
+}
